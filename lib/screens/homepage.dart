260aa2bfbf1acf353b24f4c6d75f6e45b5d2ec20
princeGedeon <guedjegedeon03@gmail.com>
2023-01-21 17:29:57 +0100

    Update

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -12,11 +12,17 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar( toolbarHeight: 70,leading:Image.asset("assets/img/logo.jpg",),title: AppText("Liste des codes",color: Colors.white,size: 18,),),
       drawer: Drawer(),
       body: Center(
         child: Text("Hello"),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+
+        onPressed: () {
+        print('hello');
+      },),
     );
   }
 }
