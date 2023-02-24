a1c482bfb7daec61fd29593dff383c7057bd1ffc
GodWindows <110693163+GodWindows@users.noreply.github.com>
2023-02-24 15:32:17 +0100

    still on the home page and added categories haha

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -3,6 +3,8 @@ import 'package:tpcoursapi/components/TodoTile.dart';
 import 'package:tpcoursapi/components/app_text.dart';
 import 'package:tpcoursapi/data/models/todo.dart';
 import 'package:tpcoursapi/data/services/todoService.dart';
+import 'package:tpcoursapi/screens/addTodo.dart';
+import 'package:tpcoursapi/utils/app_func.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,6 +14,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  bool isloading = true;
   var myTasks;
   String nbr = "...";
   @override
@@ -25,6 +28,7 @@ class _HomeScreenState extends State<HomeScreen> {
     myTasks = await ToDoService.fetch();
     setState(() {
       nbr = myTasks.length.toString();
+      isloading = false;
     });
   }
 
@@ -33,145 +37,160 @@ class _HomeScreenState extends State<HomeScreen> {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Center(
-              child: Card(
-                color: Color.fromARGB(255, 207, 98, 134),
-                child: SizedBox(
-                  width: width * 0.8,
-                  height: 100,
-                  child: Center(child: Text("Nombre total de tâches : $nbr")),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text("Tâches non commencées"),
-            Container(
-              margin: EdgeInsets.only(top: 20),
-              height: 50,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  final Todo item = myTasks[index];
-                  if (item.begined_at == null) {
-                    print(1);
-                    return Card(
-                      color: Color.fromARGB(255, 72, 77, 117),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        print("add");
+        navigateToNextPage(context, AddTodoView());
+      }),
+      body: (!isloading)
+          ? SingleChildScrollView(
+              child: Column(
+                children: [
+                  Center(
+                    child: Card(
+                      color: Color.fromARGB(255, 207, 98, 134),
                       child: SizedBox(
-                        /* 
-                        width: width * 0.4, */
+                        width: width * 0.8,
                         height: 100,
-                        child: Container(
-                            margin: EdgeInsets.all(5), child: Text(item.title)),
+                        child: Center(
+                            child: Text("Nombre total de tâches : $nbr")),
                       ),
-                    );
-                  } else {
-                    return Container();
-                  }
-                }, //41171075
-                itemCount: myTasks.length,
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text("Tâches en cours"),
-            Container(
-              margin: EdgeInsets.only(top: 20),
-              height: 50,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  final Todo item = myTasks[index];
-                  if (item.begined_at != null && item.finished_at == null) {
-                    print(1);
-                    return Card(
-                      color: Color.fromARGB(255, 72, 77, 117),
-                      child: SizedBox(
-                        /* 
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text("Tâches non commencées"),
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    height: 50,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        final Todo item = myTasks[index];
+                        if (item.begined_at == null) {
+                          print(1);
+                          return Card(
+                            color: Color.fromARGB(255, 72, 77, 117),
+                            child: SizedBox(
+                              /* 
                         width: width * 0.4, */
-                        height: 100,
-                        child: Container(
-                            margin: EdgeInsets.all(5), child: Text(item.title)),
-                      ),
-                    );
-                  } else {
-                    return Container();
-                  }
-                }, //41171075
-                itemCount: myTasks.length,
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text("Tâches terminées"),
-            Container(
-              margin: EdgeInsets.only(top: 20),
-              height: 50,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  final Todo item = myTasks[index];
-                  if (item.begined_at != null && item.finished_at != null) {
-                    print(1);
-                    return Card(
-                      color: Color.fromARGB(255, 72, 77, 117),
-                      child: SizedBox(
-                        /* 
+                              height: 100,
+                              child: Container(
+                                  margin: EdgeInsets.all(5),
+                                  child: Text(item.title)),
+                            ),
+                          );
+                        } else {
+                          return Container();
+                        }
+                      }, //41171075
+                      itemCount: myTasks.length,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text("Tâches en cours"),
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    height: 50,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        final Todo item = myTasks[index];
+                        if (item.begined_at != null &&
+                            item.finished_at == null) {
+                          print(1);
+                          return Card(
+                            color: Color.fromARGB(255, 72, 77, 117),
+                            child: SizedBox(
+                              /* 
                         width: width * 0.4, */
-                        height: 100,
-                        child: Container(
-                            margin: EdgeInsets.all(5), child: Text(item.title)),
-                      ),
-                    );
-                  } else {
-                    return Container();
-                  }
-                }, //41171075
-                itemCount: myTasks.length,
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text("Tâches terminées avec retard"),
-            Container(
-              margin: EdgeInsets.only(top: 20),
-              height: 50,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  final Todo item = myTasks[index];
-                  if (item.begined_at != null &&
-                      item.finished_at != null &&
-                      DateTime.parse(item.deadline_at)
-                              .compareTo(DateTime.parse(item.finished_at!)) <
-                          0) {
-                    return Card(
-                      color: Color.fromARGB(255, 72, 77, 117),
-                      child: SizedBox(
-                        /* 
+                              height: 100,
+                              child: Container(
+                                  margin: EdgeInsets.all(5),
+                                  child: Text(item.title)),
+                            ),
+                          );
+                        } else {
+                          return Container();
+                        }
+                      }, //41171075
+                      itemCount: myTasks.length,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text("Tâches terminées"),
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    height: 50,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        final Todo item = myTasks[index];
+                        if (item.begined_at != null &&
+                            item.finished_at != null) {
+                          print(1);
+                          return Card(
+                            color: Color.fromARGB(255, 72, 77, 117),
+                            child: SizedBox(
+                              /* 
                         width: width * 0.4, */
-                        height: 100,
-                        child: Container(
-                            margin: EdgeInsets.all(5), child: Text(item.title)),
-                      ),
-                    );
-                  } else {
-                    return Container();
-                  }
-                }, //41171075
-                itemCount: myTasks.length,
+                              height: 100,
+                              child: Container(
+                                  margin: EdgeInsets.all(5),
+                                  child: Text(item.title)),
+                            ),
+                          );
+                        } else {
+                          return Container();
+                        }
+                      }, //41171075
+                      itemCount: myTasks.length,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text("Tâches terminées avec retard"),
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    height: 50,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        final Todo item = myTasks[index];
+                        if (item.begined_at != null &&
+                            item.finished_at != null &&
+                            DateTime.parse(item.deadline_at).compareTo(
+                                    DateTime.parse(item.finished_at!)) <
+                                0) {
+                          return Card(
+                            color: Color.fromARGB(255, 72, 77, 117),
+                            child: SizedBox(
+                              /* 
+                        width: width * 0.4, */
+                              height: 100,
+                              child: Container(
+                                  margin: EdgeInsets.all(5),
+                                  child: Text(item.title)),
+                            ),
+                          );
+                        } else {
+                          return Container();
+                        }
+                      }, //41171075
+                      itemCount: myTasks.length,
+                    ),
+                  )
+                ],
               ),
             )
-          ],
-        ),
-      ),
+          : Center(
+              child: CircularProgressIndicator(),
+            ),
     );
   }
 }
